d057f037253c380ffb8cc3e6526284f9e63e08f1
KazunaSakai <s.kazuna0401@gmail.com>
2026-05-28 11:57:14 +0900

    input

---
+++ src/main/java/Input_Page/InputServlet.java
@@ -0,0 +1,46 @@
+package Input_Page; // フォルダ構成に合わせて「input」にする
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/input")
+public class InputServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        // フォーム画面を表示する
+        request.getRequestDispatcher("/Input.jsp").forward(request, response);
+    }
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        
+        request.setCharacterEncoding("UTF-8");
+
+        // データ取得
+        String oname = request.getParameter("oname");
+        String mail = request.getParameter("mail_address");
+        String sex = request.getParameter("sex");
+        String[] cates = request.getParameterValues("cates");
+        String pref = request.getParameter("pref");
+        String message = request.getParameter("message");
+
+        // 確認画面へ渡すためのセット
+        request.setAttribute("oname", oname);
+        request.setAttribute("mail_address", mail);
+        request.setAttribute("sex", sex);
+        request.setAttribute("cates", cates);
+        request.setAttribute("pref", pref);
+        request.setAttribute("message", message);
+
+        // confirm.jsp へ移動
+        request.getRequestDispatcher("/confirm.jsp").forward(request, response);
+    }
+}
+
---
+++ src/main/webapp/Input.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>確認画面</title>
+	</head>
+<body>
+    <h1>入力内容の確認</h1>
+		<form action="input" method="POST">
+		    名前: <input type="text" name="onamae" required><br>
+		    メール: <input type="email" name="mail_address" required><br>
+		    性別: <input type="radio" name="sex" value="男性">男性
+		          <input type="radio" name="sex" value="女性">女性<br>
+		    お問い合わせ種別: 
+		          <input type="checkbox" name="cates" value="製品について">製品について
+		          <input type="checkbox" name="cates" value="サービスについて">サービスについて<br>
+		    住まいエリア: 
+		          <select name="pref">
+		              <option value="北海道">北海道</option>
+		              <option value="関東">関東</option>
+		          </select><br>
+		    内容: <textarea name="message" required></textarea><br>
+		    <input type="submit" value="確認画面へ">
+</form>
